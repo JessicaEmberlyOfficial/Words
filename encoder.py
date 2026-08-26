184a1e3739fa6a790be3b1fca219591ad23b7a24
Jessica Emberly <jessicaemberly1+github@gmail.com>
2026-08-26 01:32:41 -0500

    Add clear screen command before printing output

--- encoder.py
+++ encoder.py
@@ -5,4 +5,5 @@ def encode():
   dictionary = {"a": " hell", "b": " six", "c": " sex", "d": " health", "e": " fix", "f": " spirit", "g": " denounce", "h": " basketball", "i": " football", "j": " soccerball", "k": " doom", "l": " park", "m": " bird", "n": " wildlife", "o": " elderly", "p": " extra", "q": " recreational", "r": " word", "s": " prop", "t": " game", "u": " okay", "v": " duty", "w": " computer", "x": " test", "y": " paper", "z": " life"}
   table = str.maketrans(dictionary)
   ev = encode.translate(table)
+  os.system("clear")
   print(ev)
